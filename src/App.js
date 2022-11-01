3b15ffa60ceb1aa5e91a7b1172bb2550ff890c4d
mohabElhpashy <mohab.hafez332@gmail.com>
2022-11-02 01:51:22 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import './App.css';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import Login from './pages/Login/Login';
-import Layout from './Layout/Layout';
-import {Provider} from 'react-redux'
-  import ProtectedRoutes from './Auth/Auth'
- import STORE from './redux/Store'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Layout from "./Layout/Layout";
+import { Provider } from "react-redux";
+import ProtectedRoutes from "./Auth/Auth";
+import STORE from "./redux/Store";
 
- function App() {
+function App() {
   return (
     <Provider store={STORE}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route element={<ProtectedRoutes />}>
+              <Route element={<Layout />} path="/dashoard_user/*" />
 
- <div className='App'>
-    <Router>
-       <Routes>
-          <Route element={<ProtectedRoutes/>}>
-            <Route element={<Layout/>} path="/dashoard_user/*"/>
+              {/* <Route element={<Products/>} path="/products"/> */}
+            </Route>
 
-            {/* <Route element={<Products/>} path="/products"/> */}
-          </Route>
-        
-          <Route element={<Login/>} path="/"/>
-        </Routes>
-    </Router>
-
- </div>
- </Provider>
-
+            <Route element={<Login />} path="/" />
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
--- src/pages/Pieces/Pieces.js
+++ src/pages/Pieces/Pieces.js
@@ -1,41 +1,44 @@
 import React from "react";
 
-const Pieces =()=>{
-const Pieces_Pieces=["one","two","three","four","Five","Six"]
-    return(
-        <div >
-                    <div className="container   m-auto p-4 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 align-middle py-6 justify-items-center gap-8">
-
-            {
-                Pieces_Pieces.map((ele,index)=>(
-                    <div className="bg-green-600	
-                    h-14 w-64
+const Pieces = () => {
+  const Pieces_Pieces = ["one", "two", "three", "four", "Five", "Six"];
+  return (
+    <div>
+      <div className="container   m-auto p-4 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 align-middle py-6 justify-items-center gap-14">
+        {Pieces_Pieces.map((ele, index) => (
+          <div
+            className="bg-slate-600	
+                    h-40 w-64
                     rounded-md
                     border-slate-800
                     py-2
-                    before:content[''] before:w-full before:h-1 before:bg-sky-500
+                    before:content[''] before:w-full before:h-1 before:bg-black
              relative before:absolute before:top-0 before:left-0 overflow-hidden
-                    ">
-                        
-                       <h1 className="text-slate-200 decoration-double
-"> {ele}</h1>
-                       <div style={{display:"flex",justifyContent:"space-around"}}>
-                       <button className="bg-white ">Add first Visit</button>
-                       <button className="bg-white ">Add sec Visit</button>
-                       <button className="bg-white ">Add th Visit</button>
-
-                       </div>
-
-                       
-                    
-                    
-                    
-                    </div>
-                ))
-            }
-
-                    </div>
-        </div>
-    )
-}
-export default Pieces
+                    "
+          >
+            <h1
+              className="text-slate-200 decoration-double
+              bg-cyan-700
+"
+            >
+              {" "}
+              {ele}
+            </h1>
+            <div className="flex flex-col justify-evenly h-28 container">
+              <button className="bg-white rounded-md h-8 font-bold  pointer-events-auto">
+                Add first Visit
+              </button>
+              <button className="bg-white rounded-md font-bold	h-8">
+                Add sec Visit
+              </button>
+              <button className="bg-white rounded-md  font-bold  h-8	">
+                Add th Visit
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Pieces;
